349d82f9aa3cef77bfbcf69b54841eb604e84375
wezmodo <wezmodo@hotmail.com>
2017-09-10 20:41:02 +0100

    Added the listener details

--- assets/elb.tf
+++ assets/elb.tf
@@ -1,3 +1,11 @@
-resource "aws_elb" "name" {
-  
+resource "aws_elb" "web-elb" {
+  name                    = "web-elb-test"
+  availability_zones      = ["S{data.aws_availability_zones.all.names}"]  
+
+  listener {
+    lb_port               = 80
+    lb_protocol           = "http"
+    instance_port         = "${var.server_port}"
+    instance_protcol      = "tcp"
+  }
 }
